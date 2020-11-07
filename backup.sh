ac3c1c8cdefb69f64d7899c49cec3d646e703043
Official21A <officialamirhossein21@gmail.com>
2020-11-07 08:15:00 +0330

    Backup done

--- backup.sh
+++ backup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 
-echo "process .."
+# This bash script is used to backup a user's home directory to /tmp/.
 
-tar -czf /tmp/myhome_directory.tar.gz /home/linuxconfig
+user=$(whoami)
+input=/home/$user
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
-echo "done."
+tar -czf $output $input
+echo "Backup of $input completed! Details about the output backup file:"
+ls -l $output
