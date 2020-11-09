778a5df9d1b477b8201ae0363542eb9ebb8ad4f0
Official21A <officialamirhossein21@gmail.com>
2020-11-09 07:53:00 +0330

    input

--- backup.sh
+++ backup.sh
@@ -2,7 +2,16 @@
 
 # This bash script is used to backup a user's home directory to /tmp/.
 
-user=$(whoami)
+if [ -z $1 ]; then
+        user=$(whoami)
+else
+        if [ ! -d "/home/$1" ]; then
+                echo "Requested $1 user home directory doesn't exist."
+                exit 1
+        fi
+        user=$1                                                                                                                                                                                                                                                                
+fi
+
 input=/home/$user
 output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
---
+++ varargs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# shell inputs get the strings after the file name in run time,
+# and puts in in variables from 1 to n. We can access them by $n.
+
+echo "Total input: $#"
+echo "First input: $1"
+echo "All inputs: $*"
