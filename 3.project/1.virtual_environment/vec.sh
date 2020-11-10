909c76654d94e42f33edf44e5796ffa4fc4eeb18
Official21A <officialamirhossein21@gmail.com>
2020-11-10 07:39:03 +0330

    vec create

--- 3.project/1.virtual_environment/vec.sh
+++ 3.project/1.virtual_environment/vec.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/bash
 
-echo "In progrecc"
+# a function to create a new environment
+function create_env {
+
+    if [ -z $1 ]; then
+      name=$(whoami) # if the user input was empty
+    else
+      name=$1 # set the user input name
+    fi
+
+    # dir check
+    if [ -d "./$1" ]; then
+      # if ve was already been created
+      echo "Sorry, already have a virtual environment with the name $1."
+      exit -1
+    fi
+}
