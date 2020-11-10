0e5119bca5395fced9e75baf5381ce157d5f1dc7
Official21A <officialamirhossein21@gmail.com>
2020-11-10 07:56:28 +0330

    first result

--- 3.project/1.virtual_environment/vec.sh
+++ 3.project/1.virtual_environment/vec.sh
@@ -16,5 +16,25 @@ function create_env {
       exit -1
     fi
 
-    python3 -m venv name
+    python3 -m venv name # create environment
 }
+
+
+# first we check , if user system supports python and pip ?
+if [[ "$(python3 -V)" =~ "Python 3" ]]; then
+    # this means user have python3 so we begin
+    for envs in $*; do
+        create_env $envs
+    done;
+    echo "All done, OK."
+else
+    # this mean the user system does not support python3
+    echo "System not supporting python3. Enter your pass word to install it."
+    sudo apt-get update
+    sudo apt-get install python3.6
+    sudo apt install python3-pip
+    python3 --version
+    pip3 --version
+    pip3 install virtualenv
+    echo "Python3 and pip3 installed successfully. Run the program again."
+fi
