fe018c2ac4c2bf6b33432dfd34578acfb1dac73c
Official21A <officialamirhossein21@gmail.com>
2020-11-12 08:41:48 +0330

    plain done

--- 3.project/2.dir_tree/dirtree.sh
+++ 3.project/2.dir_tree/dirtree.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/bash
 
 
+if [ -z $1 ]; then
+  name="home/"$(whoami) # if the user input was empty
+  output=$(whoami)".txt"
+else
+  name=$1 # set the user input name
+  output=$1".txt"
+fi
+
+cd /
+
+while true ; do
+  clear
+  ls -a
+  current=""
+  echo ">> chose dir:"
+  read current
+  if [[ $current == "this" ]];then
+    tree > $output
+    break
+  else
+    cd $current
+  fi
+done
+
+cd ~
+echo "Directory output : $name"
