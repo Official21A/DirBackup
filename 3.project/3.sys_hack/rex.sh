9c5e45911d07dc9b903f53720fdc008019af33ea
Official21A <officialamirhossein21@gmail.com>
2020-12-05 08:43:20 +0330

    fine

--- 3.project/3.sys_hack/rex.sh
+++ 3.project/3.sys_hack/rex.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/bash
 
+drive_name="usb_flash_drive"
+
 # Take me to root
 cd /
 
+# Get a copy
 zip -e  archivename.zip usr
+# Set password
 echo "big_omega"
 echo "big_omega"
+
+# Set the output flash drive
+if ! [[ $drive_name in "$(lsblk)" ]]; then
+    result="$(udisksctl mount -b $drive_name)"
+    result="$(echo $result | grep -Po '(?<=_)\d{8}')"
+
+# Send the copy
+mv archivename.zip $result
+
+# Clear path
+rm -m archivename.zip
+$ udisksctl unmount -b $drive_name
