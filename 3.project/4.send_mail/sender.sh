bb03980776d4b0e6ce0d8b53aa16ec3965e23157
Official21A <officialamirhossein21@gmail.com>
2020-12-06 08:47:36 +0330

    method

--- 3.project/4.send_mail/sender.sh
+++ 3.project/4.send_mail/sender.sh
@@ -1,1 +1,21 @@
-/usr/bin/bash
+#!/usr/bin/bash
+
+#Install
+sudo apt install mailutils
+# Show details
+mail -V
+
+# Get mail subject
+echo "> Enter the subject "
+read subject
+
+# Get mail body form
+echo "> Enter the mail body or file name "
+read body
+
+# Get the destination email
+echo "> To ? "
+read mail_domain
+
+# Send the mail
+mail -s $subject $mail_domain <<< $body
